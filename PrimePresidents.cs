60ca3b40cae20b266d19da736a92e633e63bbdeb
Robert White <rjwhitenh@outlook.com>
2023-03-18 16:54:29 -0400

    Switch to UKMod

---
+++ PrimePresidents.cs
@@ -0,0 +1,14 @@
+﻿using UMM;
+using UnityEngine;
+
+namespace PrimePresidents
+{
+    [UKPlugin("org.PrimePresidents","Prime Presidents", "1.0.0", "Jowari da", true, true)]
+    public class Presidents : UKMod
+    {
+        public override void OnModLoaded()
+        {
+            Debug.Log("Prime presidents starting");
+        }
+    }
+}
